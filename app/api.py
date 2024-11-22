10c41cb4cb73dbc4637b6010781e680a321f1d7a
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2024-11-22 22:09:01 +0300

    Add api server

---
+++ app/api.py
@@ -0,0 +1,29 @@
+import pickle
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from neurorag import NeuroRAG
+
+def load_docs():
+  with open('splitted_docs.pkl', 'rb') as f:
+    return pickle.load(f)
+
+docs = load_docs()
+neuro_rag = NeuroRAG(docs)
+neuro_rag.compile()
+
+app = FastAPI()
+
+class Question(BaseModel):
+  query: str
+
+@app.post("/invoke")
+async def invoke(question: Question):
+  if not question.query.strip():
+    raise HTTPException(status_code=400, detail='Question text cannot be empty.')
+
+  try:
+    answer = app.invoke(question.query)
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
+
+  return {"answer": answer}
