995ba8df3234c1a7caece791563d8d6993e44287
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2025-01-26 19:49:13 +0300

    feat: add decomposition chain

---
+++ neurorag/chains/decomposition.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel, Field
+
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.output_parsers import RetryOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnableParallel
+
+from json_extractor import JsonExtractor
+
+class DecompositionSchema(BaseModel):
+  subqueries: list[str] = Field(description='Given the original query, decompose it into 2-4 simpler sub-queries as json array of strings')
+
+template = """
+You are an AI assistant tasked with breaking down complex queries into simpler sub-queries for a RAG system.
+Given the original query, decompose it into 2-4 simpler sub-queries that, when answered together, would provide a comprehensive response to the original query.
+
+Original query: {query}
+
+example: What are the impacts of climate change on the environment?
+
+Sub-queries:
+1. What are the impacts of climate change on biodiversity?
+2. How does climate change affect the oceans?
+3. What are the effects of climate change on agriculture?
+4. What are the impacts of climate change on human health?
+
+{format_instructions}
+"""
+
+parser = PydanticOutputParser(pydantic_object=DecompositionSchema)
+
+class DecompositionChain:
+  def __init__(self, llm):
+    retry_parser = RetryOutputParser.from_llm(
+      parser=parser,
+      llm=llm,
+      max_retries=3,
+    )
+    prompt = PromptTemplate(
+      template=template,
+      input_variables=['query'],
+      partial_variables={'format_instructions': parser.get_format_instructions()},
+    )
+
+    self.chain = RunnableParallel(
+        completion=prompt | llm | JsonExtractor(), prompt_value=prompt
+    ) | RunnableLambda(lambda x: retry_parser.parse_with_prompt(**x))
+
+  def invoke(self, query: str) -> str:
+    return self.chain.invoke({'query': query})
