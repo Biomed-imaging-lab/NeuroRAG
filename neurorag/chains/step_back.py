4cf9b1ff04b14f429de24aa702c20af8f50d024e
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2025-01-26 19:37:46 +0300

    feat: add step back chain

---
+++ neurorag/chains/step_back.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, Field
+
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.output_parsers import RetryOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda, RunnableParallel
+
+from json_extractor import JsonExtractor
+
+class StepBackSchema(BaseModel):
+  step_back: str = Field(description="Given the original query, generate a step-back query that is more general and can help retrieve relevant background information.")
+
+template = """
+You are an AI assistant tasked with generating broader, more general queries to improve context retrieval in a RAG system.
+Given the original query, generate a step-back query that is more general and can help retrieve relevant background information.
+
+{format_instructions}
+
+Original query: {query}
+
+Step-back query:
+"""
+
+parser = PydanticOutputParser(pydantic_object=StepBackSchema)
+
+class StepBackChain:
+  def __init__(self, llm):
+    retry_parser = RetryOutputParser.from_llm(
+      parser=parser,
+      llm=llm,
+      max_retries=3,
+    )
+    prompt = PromptTemplate(
+      template=template,
+      input_variables=['query'],
+      partial_variables={'format_instructions': parser.get_format_instructions()},
+    )
+
+    self.chain = RunnableParallel(
+        completion=prompt | llm | JsonExtractor(), prompt_value=prompt
+    ) | RunnableLambda(lambda x: retry_parser.parse_with_prompt(**x))
+
+  def invoke(self, query: str) -> str:
+    return self.chain.invoke({'query': query})
