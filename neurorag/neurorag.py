02c1de7231550233fd2dc935041e0ccce52d18e0
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2025-01-27 01:11:50 +0300

    feat: update NeuroRAG class

---
+++ neurorag/neurorag.py
@@ -0,0 +1,442 @@
+import re
+import operator
+from typing import Annotated
+from typing_extensions import TypedDict
+
+from langchain.schema import Document
+from langchain_chroma import Chroma
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.embeddings.cache import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain_community.llms import Ollama
+from langgraph.graph import START, END, StateGraph
+from langchain_community.retrievers import PubMedRetriever, ArxivRetriever
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+from chains.route import RouteChain
+from chains.document_grade import DocumentGradeChain
+from chains.hallucinations import HallucinationsChain
+from chains.answer_grade import AnswerGradeChain
+from chains.hyde import HyDEChain
+from chains.step_back import StepBackChain
+from chains.query_rewriting import QueryRewritingChain
+from chains.decomposition import DecompositionChain
+from chains.ncbi_protein import NCBIProteinChain
+from chains.ncbi_gene import NCBIGeneChain
+from chains.generation import GenerationChain
+
+class GraphStateSchema(TypedDict):
+  question: str
+
+  specialized_sources: list[str]
+
+  step_back_query: str
+  rewritten_query: str
+  subqueries: list[str]
+
+  generated_documents: list[str]
+
+  documents: Annotated[list, operator.add]
+
+  web_search: str
+
+  generation: str
+  generations_number: int
+
+class NeuroRAG():
+  def __init__(self, temperature=0):
+    self.temperature = temperature
+
+  def compile(self):
+    self.llm = Ollama(model='llama3.1', temperature=self.temperature)
+
+    embeddings = OllamaEmbeddings(model='llama3.1')
+    embeddings_store = LocalFileStore('./.embeddings_cache')
+    self.embeddings = CacheBackedEmbeddings.from_bytes_store(
+      embeddings,
+      embeddings_store,
+      namespace=embeddings.model,
+    )
+
+    self.vector_store = Chroma(
+      collection_name='neurorag',
+      embedding_function=self.embeddings,
+      persist_directory='./chroma_db',
+    )
+    self.vector_store_retriever = self.vector_store.as_retriever()
+    self.pub_med_retriever = PubMedRetriever()
+    self.arxiv_retriever = ArxivRetriever(
+      load_max_docs=3,
+      get_ful_documents=True,
+    )
+    self.ncbi_protein_retriever = NCBIProteinRetriever(k=3)
+
+    self.route_chain = RouteChain(self.llm)
+    self.hyde_chain = HyDEChain(self.llm)
+    self.step_back_chain = StepBackChain(self.llm)
+    self.query_rewrite_chain = QueryRewritingChain(self.llm)
+    self.decomposition_chain = DecompositionChain(self.llm)
+    self.ncbi_protein_db_chain = NCBIProteinChain(self.llm)
+    self.ncbi_gene_db_chain = NCBIGeneChain(self.llm)
+    self.docs_grader_chain = DocumentGradeChain(self.llm)
+    self.web_search_chain = TavilySearchResults(k=5)
+    self.rag_chain = GenerationChain()
+    self.hallucinations_grader_chain = HallucinationsChain(self.llm)
+    self.answer_grader_chain = AnswerGradeChain(self.llm)
+
+    workflow = StateGraph(GraphStateSchema)
+
+    workflow.add_node('determine_specialized_sources', self.determine_specialized_src_node)
+
+    workflow.add_node('generate_step_back_query', self.generate_step_back_query_node)
+    workflow.add_node('generate_rewritten_query', self.generate_rewritten_query_node)
+    workflow.add_node('generate_subqueries', self.generate_subqueries_node)
+
+    workflow.add_node('generate_hyde_docs', self.generate_hyde_docs_node)
+
+    workflow.add_node('vector_store_retriever', self.vector_store_retriever_node)
+    workflow.add_node('pub_med_retriever', self.pub_med_retriever_node)
+    workflow.add_node('arxiv_retriever', self.arxiv_retriever_node)
+    workflow.add_node('ncbi_protein_db_retriever', self.ncbi_protein_db_retriever_node)
+    workflow.add_node('ncbi_gene_db_retriever', self.ncbi_gene_db_retriever_node)
+
+    workflow.add_node('websearch', self.web_search_node)
+    workflow.add_node('generate', self.generate_node)
+    workflow.add_node('grade_documents', self.grade_documents_node)
+
+    workflow.add_edge(START, 'determine_specialized_sources')
+    workflow.add_conditional_edges(
+      'determine_specialized_sources',
+      self.route_question_node,
+      {
+        'websearch': 'websearch',
+        'specialized_sources': 'generate_step_back_query',
+      },
+    )
+
+    workflow.add_edge('generate_step_back_query', 'generate_rewritten_query')
+    workflow.add_edge('generate_rewritten_query', 'generate_subqueries')
+    workflow.add_edge('generate_subqueries', 'generate_hyde_docs')
+
+    workflow.add_edge('generate_hyde_docs', 'vector_store_retriever')
+    workflow.add_edge('generate_hyde_docs', 'pub_med_retriever')
+    workflow.add_edge('generate_hyde_docs', 'arxiv_retriever')
+    workflow.add_edge('generate_hyde_docs', 'ncbi_protein_db_retriever')
+    workflow.add_edge('generate_hyde_docs', 'ncbi_gene_db_retriever')
+
+    workflow.add_edge('vector_store_retriever', 'grade_documents')
+    workflow.add_edge('pub_med_retriever', 'grade_documents')
+    workflow.add_edge('arxiv_retriever', 'grade_documents')
+    workflow.add_edge('ncbi_protein_db_retriever', 'grade_documents')
+    workflow.add_edge('ncbi_gene_db_retriever', 'grade_documents')
+
+    workflow.add_conditional_edges(
+      'grade_documents',
+      self.decide_to_generate_node,
+      {
+        'websearch': 'websearch',
+        'generate': 'generate',
+      },
+    )
+    workflow.add_edge('websearch', 'generate')
+    workflow.add_conditional_edges(
+      'generate',
+      self.grade_generation_node,
+      {
+        'not supported': 'generate',
+        'useful': END,
+        'not useful': 'websearch',
+      },
+    )
+
+    self.app = workflow.compile()
+
+  def invoke(self, question):
+    result = self.app.invoke({'question': question})
+    return result
+
+  def extract_json_parser(self, response):
+    json_pattern = r'\{.*?\}'
+    match = re.search(json_pattern, response, re.DOTALL)
+
+    if match:
+      return match.group().strip()
+
+    return response
+
+  def route_question_node(self, state):
+    print('---ROUTE QUESTION---')
+
+    sources = state['specialized_sources']
+
+    if len(sources) == 0:
+      print('---ROUTE QUESTION TO WEB SEARCH---')
+      return 'websearch'
+    else:
+      print(f'---ROUTE QUESTION TO SPECIALIZED SOURCES: {', '.join([source.upper() for source in sources])}---')
+      return 'specialized_sources'
+
+  def generate_step_back_query_node(self, state):
+    print('---GENERATE STEP-BACK QUERY---')
+
+    question = state['question']
+    step_back_query = self.step_back_chain.invoke({'question': question})
+    return {'step_back_query': step_back_query}
+
+  def generate_rewritten_query_node(self, state):
+    print('---GENERATE REWRITTEN QUERY---')
+
+    question = state['question']
+    rewritten_query = self.query_rewrite_chain.invoke({'question': question})
+    return {'rewritten_query': rewritten_query}
+
+  def generate_subqueries_node(self, state):
+    print('---GENERATE SUBQUERIES---')
+
+    question = state['question']
+
+    try:
+      decomposition_answer = self.decomposition_chain.invoke({'question': question})
+      subqueries = decomposition_answer.subqueries
+      # Limit to a maximum of four subqueries
+      subqueries = subqueries[:4]
+    except:
+      subqueries = []
+
+    print(f'---FINAL SUBQUERIES NUMBER: {len(subqueries)}---')
+
+    return {'subqueries': subqueries}
+
+  def generate_hyde_docs_node(self, state):
+    print('---GENERATE HYDE DOCUMENTS---')
+
+    question = state['question']
+    step_back_query = state['step_back_query']
+    rewritten_query = state['rewritten_query']
+    subqueries = state['subqueries']
+
+    queries = [question, step_back_query, rewritten_query, *subqueries]
+    generated_documents = []
+
+    for query in queries:
+      generated_document = self.hyde_chain.invoke({'question': query})
+      generated_documents.append(generated_document)
+
+    return {'question': question, 'generated_documents': generated_documents}
+
+  def vector_store_retriever_node(self, state):
+    generated_documents = state['generated_documents']
+    specialized_sources = state['specialized_sources']
+
+    if 'vectorstore' not in specialized_sources:
+      return {'documents': []}
+
+    print('---RETRIEVE FROM VECTOR STORE---')
+
+    documents = []
+
+    for generated_document in generated_documents:
+      documents.extend(self.vector_store_retriever.invoke(generated_document))
+
+    return {'documents': documents}
+
+  def pub_med_retriever_node(self, state):
+    generated_documents = state['generated_documents']
+    specialized_sources = state['specialized_sources']
+
+    if 'pubmed' not in specialized_sources:
+      return {'documents': []}
+
+    print('---RETRIEVE FROM PUBMED---')
+
+    documents = []
+
+    for generated_document in generated_documents:
+      try:
+        documents.extend(self.pub_med_retriever.invoke(generated_document))
+      except:
+        pass
+
+    return {'documents': documents}
+
+  def arxiv_retriever_node(self, state):
+    generated_documents = state['generated_documents']
+    specialized_sources = state['specialized_sources']
+
+    if 'arxiv' not in specialized_sources:
+      return {'documents': []}
+
+    print('---RETRIEVE FROM ARXIV---')
+
+    documents = []
+
+    for generated_document in generated_documents:
+      try:
+        documents.extend(self.arxiv_retriever.invoke(generated_document))
+      except:
+        pass
+
+    return {'documents': documents}
+
+  def ncbi_protein_db_retriever_node(self, state):
+    specialized_sources = state['specialized_sources']
+
+    if 'ncbi_protein' not in specialized_sources:
+      return {'documents': []}
+
+    print('---RETRIEVE FROM NCBI PROTEIN DB---')
+
+    question = state['question']
+    step_back_query = state['step_back_query']
+    rewritten_query = state['rewritten_query']
+    subqueries = state['subqueries']
+
+    queries = [question, step_back_query, rewritten_query, *subqueries]
+    documents = []
+
+    for query in queries:
+      try:
+        documents.extend(self.ncbi_protein_db_chain.invoke(query))
+      except:
+        pass
+
+    return {'documents': documents}
+
+  def ncbi_gene_db_retriever_node(self, state):
+    specialized_sources = state['specialized_sources']
+
+    if 'ncbi_gene' not in specialized_sources:
+      return {'documents': []}
+
+    print('---RETRIEVE FROM NCBI GENE DB---')
+
+    question = state['question']
+    step_back_query = state['step_back_query']
+    rewritten_query = state['rewritten_query']
+    subqueries = state['subqueries']
+
+    queries = [question, step_back_query, rewritten_query, *subqueries]
+    documents = []
+
+    for query in queries:
+      try:
+        documents.extend(self.ncbi_gene_db_chain.invoke(query))
+      except:
+        pass
+
+    return {'documents': documents}
+
+  def grade_documents_node(self, state):
+    print('---CHECK DOCUMENT RELEVANCE TO QUESTION---')
+
+    question = state['question']
+    documents = state['documents']
+
+    print(f'---INITIAL DOCUMENTS NUMBER: {len(documents)}---')
+
+    filtered_documents = []
+    seen_contents = set()
+    web_search = 'No'
+
+    for index, document in enumerate(documents):
+      print(f'---GRADE DOCUMENT ({index + 1}/{len(documents)})---')
+
+      if document.page_content in seen_contents:
+        print('---GRADE: DOCUMENT IS REPEATED---')
+        continue
+      seen_contents.add(document.page_content)
+
+      try:
+        score = self.docs_grader_chain.invoke({
+          'question': question,
+          'document': document.page_content,
+        })
+        grade = score.binary_score
+      except:
+        grade = 'no'
+
+      if grade.lower() == 'yes':
+        print('---GRADE: DOCUMENT RELEVANT---')
+        filtered_documents.append(document)
+      else:
+        print('---GRADE: DOCUMENT NOT RELEVANT---')
+        web_search = 'Yes'
+        continue
+
+    print(f'---FINAL DOCUMENTS NUMBER: {len(filtered_documents)}---')
+
+    state['documents'].clear()
+    return {
+      'documents': filtered_documents,
+      'web_search': web_search,
+    }
+
+  def decide_to_generate_node(self, state):
+    web_search = state['web_search']
+    return 'websearch' if web_search == 'Yes' else 'generate'
+
+  def web_search_node(self, state):
+    print('---WEB SEARCH---')
+
+    question = state['question']
+
+    web_results = self.web_search_chain.invoke({'query': question})
+    docs = [Document(page_content=result['content'], metadata={'source': result['url']}) for result in web_results]
+
+    return {'documents': docs}
+
+  def generate_node(self, state):
+    print('---GENERATE---')
+
+    question = state['question']
+    documents = state['documents']
+    generations_number = state.get('generations_number', 0)
+
+    context = '\n\n' + '\n\n'.join(map(lambda doc: doc.page_content, documents)) + '\n\n'
+    generation = self.rag_chain.invoke({'context': context, 'question': question})
+
+    return {'generation': generation, 'generations_number': generations_number + 1}
+
+  def grade_generation_node(self, state):
+    print('---CHECK HALLUCINATIONS---')
+
+    question = state['question']
+    documents = state['documents']
+    generation = state['generation']
+    generations_number = state['generations_number']
+
+    if generations_number >= 2:
+      return 'useful'
+
+    try:
+      context = '\n\n' + '\n\n'.join(map(lambda doc: doc.page_content, documents)) + '\n\n'
+      score = self.hallucinations_grader_chain.invoke({
+        'documents': context,
+        'generation': generation,
+      })
+      grade = score.binary_score
+    except:
+      grade = 'no'
+
+    if grade == 'yes':
+      print('---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---')
+      print('---GRADE GENERATION vs QUESTION---')
+
+      try:
+        score = self.answer_grader_chain.invoke({
+          'question': question,
+          'generation': generation,
+        })
+        grade = score.binary_score.lower()
+      except:
+        grade = 'no'
+
+      if grade == 'yes':
+        print('---DECISION: GENERATION ADDRESSES QUESTION---')
+        return 'useful'
+      else:
+        print('---DECISION: GENERATION DOES NOT ADDRESS QUESTION---')
+        return 'not useful'
+    else:
+      print('---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---')
+      return 'not supported'
